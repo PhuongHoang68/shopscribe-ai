730f1e353ca8321f264235a1e179d1234842a781
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2025-07-08 18:04:20 -0500

    functioning form, but going to try adding additional animation

--- src/app/intake/layout.js
+++ src/app/intake/layout.js
@@ -11,11 +11,12 @@ export default function IntakeLayout({ children }) {
     // <div className="mx-auto mt-8 border rounded-lg shadow-lg">
 <div className="relative min-h-[85vh]">
       <div className="intake-background row-span-full"></div>
-
-      {/* <nav className="flex border-b content-on-hero pl-[6%] pt-[13%]"> */}
       <div className="flex items-start content-on-hero">
-    <div className="w-[600px] mx-4 mt-8 mb-8 border rounded-lg shadow-lg bg-white" style={{height: "650px"}}>
-      <nav className="flex border-b px-6 py-4">
+
+    <div className="w-[600px] mx-4 mt-8  border-[#ECECEC] rounded-lg shadow-lg bg-white" style={{height: "650px"}}>
+    <nav className='tabs-bar'>
+      {/* <nav className="flex border-b px-6 py-4"> */}
+      
 
         {[
           { label: 'Shop', href: '/intake/create-shop', active: isShop },
@@ -26,8 +27,8 @@ export default function IntakeLayout({ children }) {
             href={tab.href}
             className={`px-4 py-2 -mb-px font-medium ${
               tab.active
-                ? 'border-b-2 border-blue-600 text-blue-600'
-                : 'text-gray-600 hover:text-blue-500'
+              ? 'bg-white border-x border-t border-[#ECECEC] text-blue-600 rounded-t-xl' // Active
+              : 'bg-transparent text-gray-600 hover:bg-[#DFDFDF] hover:text-blue-600 rounded-t-md' // Inactive + hover
             }`}
           >
             {tab.label}
@@ -36,6 +37,7 @@ export default function IntakeLayout({ children }) {
       </nav>
 
       <div className="p-4" style={{height: "650px"}}>
+      {/* <div className='content-box'> */}
         {children}
       </div>
     </div>
